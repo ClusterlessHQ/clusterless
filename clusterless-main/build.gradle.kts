1ac92d49bd2b5daa921e3436bcd9f97955521bbd
Chris K Wensel <chris@wensel.net>
2023-07-05 10:27:23 -0700

    retain project name in distribution archive

--- clusterless-main/build.gradle.kts
+++ clusterless-main/build.gradle.kts
@@ -10,17 +10,9 @@ import org.jreleaser.model.Active
 import org.jreleaser.model.Distribution
 import org.jreleaser.model.Stereotype
 
-/*
- * Copyright (c) 2023 Chris K Wensel <chris@wensel.net>. All Rights Reserved.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
 plugins {
     id("clusterless.java-application-conventions")
-    id("org.jreleaser") version "1.6.0"
+    id("org.jreleaser") version "1.7.0"
 }
 
 dependencies {
@@ -42,6 +34,7 @@ tasks.getAt("installDist").dependsOn(awsKernelInstall)
 
 distributions {
     main {
+        distributionBaseName.set("clusterless")
         contents {
             from(awsKernelInstall) {
                 include("bin/")
@@ -59,9 +52,9 @@ jreleaser {
         copyright.set("Chris K Wensel")
         license.set("MPL-2.0")
         stereotype.set(Stereotype.CLI)
-
         links {
             homepage.set("https://github.com/ClusterlessHQ")
+            documentation.set("https://docs.clusterless.io/")
         }
         inceptionYear.set("2023")
         gitRootSearch.set(true)
@@ -87,8 +80,11 @@ jreleaser {
     }
 
     distributions {
-        create("cls") {
+        create("clusterless") {
             distributionType.set(Distribution.DistributionType.JAVA_BINARY)
+            executable {
+                name.set("cls")
+            }
             artifact {
                 path.set(file("build/distributions/{{distributionName}}-{{projectVersion}}.zip"))
             }
@@ -98,7 +94,5 @@ jreleaser {
 
 tasks.register("release") {
     dependsOn("distZip")
-    dependsOn("jreleaserConfig")
-    dependsOn("jreleaserAssemble")
     dependsOn("jreleaserRelease")
 }
